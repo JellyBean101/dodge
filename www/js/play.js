e7be3ed41f7e5b0684d8d4285e36012d25fb083d
Owen Paquette <owenjpaquette@gmail.com>
2016-03-03 18:28:59 -0500

    save

---
+++ www/js/play.js
@@ -0,0 +1,24 @@
+// State: DogeIsAwesome.Play
+
+'use strict'
+
+var DogeIsAwesome = {};
+
+DogeIsAwesome.Play = function () {};
+
+DogeIsAwesome.Play.prototype = {
+
+  init: function () {
+    console.log("%c Doge is good at dodging, so he has serious skills in that matter. \n Doge is slow (when he isnt dodging), so wait a little ;).", "color:#073642")
+  },
+
+  preload: function () {
+  },
+
+  create: function () {
+  },
+
+  update: function () {
+  }
+
+};
