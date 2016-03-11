c2269cec31df5e1c1d64847b71a4eb320fd939ea
Owen Paquette <owenjpaquette@gmail.com>
2016-03-11 16:51:08 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -14,11 +14,19 @@ DogeIsAwesome.Play.prototype = {
 
   preload: function () {
     this.load.image('background','assets/Doge BG.png');
+    this.load.spritesheet('doge','assets/Dodger',38,67,1);
   },
 
   create: function () {
+    //BG
     this.background = this.add.tileSprite(0,0,320,568,'background');
     this.background.scale.set(1);
+    this.background.animations.add('blink');
+    this.background.animations.play('blink',2,true);
+    //Dodger
+    this.dodger.smoothed = false;
+    this.dodger = this.add.sprite(160,60,'dodger');
+    this.dodger.anchor.set(0.5,0.5);
   },
 
   update: function () {
